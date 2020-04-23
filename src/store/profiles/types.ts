035af9ec7362e0ab11bafef104c5d2377f7ef820
Cristian S <cristian_s@xxx.yyy>
2020-04-23 09:03:27 -0600

    Added data in src/store/profiles/types.ts.

--- src/store/profiles/types.ts
+++ src/store/profiles/types.ts
@@ -1,1 +1,24 @@
- 
+// User data structure
+export interface User {
+	id: number,
+	name: string,
+	password: string,
+	email: string,
+	phone: string,
+	age: number,
+	interests: string
+}
+
+// The collection of users
+export interface Profiles {
+	users: User[]
+}
+
+// Actions
+export const SELECT_CURRENT_USER = 'SELECT_CURRENT_USER';
+
+// Action requirements
+export interface SelectCurrentUser {
+	type: typeof SELECT_CURRENT_USER,
+	id: number		// This will match our user ID
+}
